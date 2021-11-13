5dc4380917694f72b5f8c4f46afce9c3a5b80b96
shuang <shuang.com>
2021-11-13 21:02:04 +0800

    alter file

---
+++ podRepo/Classes/TestViewController.h
@@ -0,0 +1,16 @@
+//
+//  TestViewController.h
+//  podRepo
+//
+//  Created by 刘双 on 2021/11/13.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestViewController : UIViewController
+- (void)projectConstructSuccess;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ podRepo/Classes/TestViewController.m
@@ -0,0 +1,25 @@
+//
+//  TestViewController.m
+//  podRepo
+//
+//  Created by 刘双 on 2021/11/13.
+//
+
+#import "TestViewController.h"
+
+@interface TestViewController ()
+
+@end
+
+@implementation TestViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)projectConstructSuccess{
+    NSLog(@"run success");
+}
+
+@end
